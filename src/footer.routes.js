9cff521072ce3b721eacacf0c326669a4f2e9b3b
Kassi_0958 <marvinjunior0958@gmail.com>
2025-06-03 11:38:56 +0200

    fix: remove unused GitHubIcon import

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -1,7 +1,7 @@
 // @mui icons
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
+
 import YouTubeIcon from "@mui/icons-material/YouTube";
 
 // Material Kit 2 React components
